6037e3ec4ee32bb275a34e5f04fc8427ebaaa28a
Maren Sajdaras <74674192+marensajdaras@users.noreply.github.com>
2021-09-26 12:28:21 +0200

    Output the false return
    We can't output anything if the string is not in the file , cause the condition f=1 is assigned when the condition is True and then the function end with the return statement that return True.
    With the else block in the line 27 we can now output something if the string is not in the file.

--- projects/String_search_from_multiple_files/findstring.py
+++ projects/String_search_from_multiple_files/findstring.py
@@ -19,15 +19,14 @@ def getfiles(path):
         if os.path.isfile(abs_path):
             f = open(file_name, "r")
             if text in f.read():
-                f = 1
+                
                 print(text + " found in ")
                 final_path = os.path.abspath(file_name)
                 print(final_path)
                 return True
-
-    if f == 1:
-        print(text + " not found! ")
-        return False
+            else:
+                print(text + " not found! ")
+                return False
 
 
 getfiles(path)
